4f61d396c18c73193c55e56cdf6c7089b1fece02
Bartek Zawadka <kontakt@bartoszzawadka.pl>
2018-12-29 23:43:52 +0100

    Fixed deploy config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
+let apiUrl  = 'http://localhost:5000/api';
+if(process.env.REACT_APP_API_URL){
+  apiUrl = process.env.REACT_APP_API_URL;
+}
+
 const Config = {
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: apiUrl
 };
 
 export default Config;
